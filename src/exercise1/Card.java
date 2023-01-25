819170b3f0e480fcda5a915ccc608c8533c7d3ba
BhumilPatel <pate4201@sheridancollege.ca>
2023-01-25 15:44:03 -0500

    2

--- src/exercise1/Card.java
+++ src/exercise1/Card.java
@@ -43,4 +43,11 @@ public class Card {
     public void setValue(int value) {
         this.value = value;
     }  
+    @Override
+    public String toString() {
+        return "Card{" +
+                "suit='" + suit + '\'' +
+                ", value=" + value +
+                '}';
+    }
 }
--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,5 +1,8 @@
 package exercise1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
@@ -13,9 +16,16 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] hand = new Card[7];
+         Scanner scan = new Scanner(System.in);
+        Random generator = new Random();
+        generator.setSeed(System.currentTimeMillis());
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
+            card.setValue(generator.nextInt(13) + 1);
+            card.setSuit(Card.SUITS[generator.nextInt(4)]);
+            hand[i] = card;
+           // System.out.println(card);
             //card.setValue(insert call to random number generator here)
             // 
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
@@ -23,6 +33,28 @@ public class CardTrick {
             //       Don't worry about duplicates at this point
         }
 
+         System.out.print("Enter card value: ");
+        int val = scan.nextInt();
+        System.out.print("Enter suit: ");
+        String suit = scan.next();
+        Card key = new Card();
+        key.setValue(val);
+        key.setSuit(suit);  
+        
+        //Card key = new Card();
+        key.setValue(11);
+        key.setSuit("Hearts");
+        boolean found = false;
+        System.out.println("Searching for: " + key);
+        for (int i = 0; i < hand.length; i++) {
+            if ((hand[i].getValue() == key.getValue()) && hand[i].getSuit().equalsIgnoreCase(key.getSuit())) {
+                System.out.println("Card is found in magic hand at index " + (i+1));
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+            System.out.println("Card is not found in magic hand.");
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
         // Hint: You can ask for values 1 to 10, and then
